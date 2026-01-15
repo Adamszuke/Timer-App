206b3f4807b0a5ef449795aef5b2ccbcaf13a0a1
Adamszuke <g.nakagawazuke@gmail.com>
2026-01-15 19:05:09 -0300

    feat/main-page: background-color

---
+++ Timer-App/src/styles/Colors.ts
@@ -0,0 +1,28 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export const colors = {
+  red: {
+    100: '#fca5b2',
+    200: '#f87171',
+    300: '#ef4444',
+    400: '#dc2626',
+  },
+  blue: {
+    100: '#93c5fd',
+    200: '#3b82f6',
+    300: '#2563eb',
+    400: '#1d4ed8',
+  },
+  black: {
+    100: '#4d4d4d',
+    200: '#333333#1a1a1a',
+    300: '#1a1a1a',
+    400: '#000000',
+  },
+  white: {
+    100: '#ffffff',
+    200: '#f6fcff',
+    300: '#e7feff',
+  },
+};
