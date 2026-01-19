ec6cbb5a9a75ce936f0bab405013746ef07407e6
Adamszuke <g.nakagawazuke@gmail.com>
2026-01-19 15:30:41 -0300

    feat/timer-controls - Layout Config

--- Timer-App/src/Components/atoms/pause-button.tsx
+++ Timer-App/src/Components/atoms/pause-button.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     image: {
         width: 70,
         height: 70,
-        marginTop: 20,
+        marginTop: 35,
         tintColor: colors.white[100], // Aplica a cor branca à imagem
     }
 })
