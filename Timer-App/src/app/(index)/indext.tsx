e9f0c80bb00d366750aebd7bed72de17e7367a0c
Adamszuke <g.nakagawazuke@gmail.com>
2026-01-19 15:05:24 -0300

    feat/start-button - Button Added

--- Timer-App/src/app/(index)/indext.tsx
+++ Timer-App/src/app/(index)/indext.tsx
@@ -1,14 +1,21 @@
-import {
-    Text,
-    View
-} from "react-native";
-
+import { Text, View, Alert } from "react-native";
 import { styles } from "./styles";
+import { StartButtom } from "@/Components/atoms/start-button"; // Verifique se o caminho está correto
 
 export default function IndexPage() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.tittle}>Timer App</Text>
-        </View>
-    );
+  
+  const handleStart = () => {
+    Alert.alert("Timer iniciado!");
+    // Aqui entrará a sua lógica de contagem regressiva
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tittle}>Timer App</Text>
+      
+      {/* Adicionando o botão aqui */}
+      <StartButtom onPress={handleStart} />
+      
+    </View>
+  );
 }
